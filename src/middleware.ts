6a3b9252f58e73f1fdf2e99bf86a90f228da1dc1
mk/1589 <mnirmalkar745@gmail.com>
2024-04-26 20:28:42 +0530

    addmiddleware

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+
+export default authMiddleware({
+  publicRoutes: ["/sign-in", "/sign-up", "/api/webhook", "/api/subscribe"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
